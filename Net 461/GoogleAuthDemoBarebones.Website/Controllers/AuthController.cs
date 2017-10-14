bba21dc1143702f4018c808ac74016d6db59cad8
Brian Harwell <brianharwell@gmail.com>
2017-10-14 12:13:47 -0400

    Add ability to authenticate using google

---
+++ Net 461/GoogleAuthDemoBarebones.Website/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Microsoft.Owin.Security;
+
+namespace GoogleAuthDemoBarebones.Website.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ActionResult SignIn()
+        {
+            return new GoogleAuthenticationResult();
+        }
+
+        public ActionResult Callback()
+        {
+            var externalLoginInfo = HttpContext.GetOwinContext().Authentication.GetExternalLoginInfo();
+            
+            return View(externalLoginInfo); // Don't pass the ExternlLoginInfo class to the view, this is just for demo purposes
+        }
+    }
+
+    public class GoogleAuthenticationResult : HttpUnauthorizedResult
+    {
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var authenticationProperties = new AuthenticationProperties();
+
+            authenticationProperties.RedirectUri = "/auth/callback";
+
+            context.HttpContext.GetOwinContext().Authentication.Challenge(authenticationProperties, "Google"); // Case sensitive, must be Google
+        }
+    }
+}
---
+++ Net 461/GoogleAuthDemoBarebones.Website/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Google;
+using Owin;
+
+[assembly: OwinStartup(typeof(GoogleAuthDemoBarebones.Website.Startup))]
+namespace GoogleAuthDemoBarebones.Website
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            var googleOAuth2AuthenticationOptions = new GoogleOAuth2AuthenticationOptions();
+
+            googleOAuth2AuthenticationOptions.ClientId = "<ENTER YOUR CLIENT ID>";
+            googleOAuth2AuthenticationOptions.ClientSecret = "<ENTER YOUR CLIENT SECRET>";
+
+            app.UseGoogleAuthentication(googleOAuth2AuthenticationOptions);
+        }
+    }
+}
