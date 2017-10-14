f7f5a972d6a030ff0a48233f5ee629e10852779d
Brian Harwell <brianharwell@gmail.com>
2017-10-14 10:21:24 -0400

    Add base Net 461 project
    Project uses individual accounts for authentication

---
+++ Net 461/GoogleAuthDemo.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GoogleAuthDemo.Website.MvcApplication" Language="C#" %>
---
+++ Net 461/GoogleAuthDemo.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GoogleAuthDemo.Website.Startup))]
+namespace GoogleAuthDemo.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
